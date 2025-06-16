f5ee2913bb751231466a457da9f28d87c64097fe
lucasmcar <lmc.lucas90@gmail.com>
2025-06-16 17:57:39 -0300

    Melhorando os caminhos para views

--- Source/Core/View/View.php
+++ Source/Core/View/View.php
@@ -50,7 +50,7 @@ class View
     public function render($template)
     {
         //$templatePath = dirname(__DIR__, 3) .'/resources/views/' . $template . '.tpl';
-        $templatePath = realpath('resources/views/' . $template . '.tpl');
+        $templatePath = realpath(getcwd().'/resources/views/' . $template . '.tpl');
         if (!file_exists($templatePath)) {
             throw new \Exception("Template $templatePath not found!");
         }
@@ -66,7 +66,7 @@ class View
 
         if ($this->layout) {
             //$layoutPath = dirname(__DIR__, 3).'/resources/views/layouts/' . $this->layout . '.tpl';
-            $layoutPath = realpath('resources/views/layouts/' . $this->layout . '.tpl');
+            $layoutPath = realpath(getcwd().'/resources/views/layouts/' . $this->layout . '.tpl');
     
             if (!file_exists($layoutPath)) {
                 throw new \Exception("Layout $layoutPath not found!");
