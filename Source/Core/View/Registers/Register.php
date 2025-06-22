d369a2757f4894ec6a0b18555c52d5011a1e7481
lucasmcar <lmc.lucas90@gmail.com>
2025-06-22 13:33:53 -0300

    Added base_url helper and Register class fixed security

--- Source/Core/View/Registers/Register.php
+++ Source/Core/View/Registers/Register.php
@@ -5,10 +5,10 @@ namespace Core\View\Registers;
 class Register
 {
     protected static $patterns = [
-        // Primeiro: conteúdo não escapado (consciente)
+        // Conteúdo não escapado (use com muita cautela no template)
         '/\{\{\!\s*([$\w\[\]\->\'"]+)\s*\}\}/',
 
-        // Segundo: conteúdo escapado por padrão
+        // Conteúdo escapado por padrão
         '/\{\{\s*([$\w\[\]\->\'"]+)\s*\}\}/',
 
         '/\{\%\s*if\s+(.+?)\s*\%\}/',
@@ -20,20 +20,26 @@ class Register
         '/\{\%\s*endforeach;\s*\%\}/',
         '/\{\%\s*while\s+(.+?)\s*\%\}/',
         '/\{\%\s*endwhile;\s*\%\}/',
+
         '/\{\%\s*include\s+\'(.+?)\'\s*\%\}/',
+
         '/\{\%\s*debugger\s*\(\s*(.+?)\s*\)\s*\%\}/',
-        '/@css\(\s*(.+?)\s*\)/',
+
+        '/@css\(\s*["\'](.+?)["\']\s*\)/',
         '/@js\(\s*["\'](.+?)["\']\s*\)/',
+
         '/@csrf/',
+
         '/\{\%\s*year\s*\%\}/',
+
+        // Controle de loop
         '/@for\(\s*(.+?),\s*(.+?),\s*(.+?)\s*\)/',
         '/@endfor\(\)/',
-        '/\{\s*var\s*(.+?)\s*\}/'
     ];
 
     protected static $replacements = [
-        '<?php echo $1; ?>', // {{! ... }} - sem escape
-        '<?php echo htmlspecialchars($1, ENT_QUOTES, "UTF-8"); ?>', // {{ ... }} - com escape
+        '<?php echo $1; ?>', // Conteúdo não escapado (programador assume o risco)
+        '<?php echo htmlspecialchars($1, ENT_QUOTES, "UTF-8"); ?>', // Conteúdo escapado
 
         '<?php if ($1): ?>',
         '<?php elseif ($1): ?>',
@@ -48,14 +54,22 @@ class Register
         // include seguro
         '<?php $path = realpath(__DIR__ . "/../../views/$1.tpl"); if ($path && strpos($path, realpath(__DIR__ . "/../../views")) === 0 && file_exists($path)) include $path; ?>',
 
-        '<?php print_r($1); ?>',
-        '<link rel="stylesheet" href="$1">',
-        '<script src="$1"></script>',
+        // debugger - uso interno somente
+        '<?php print_r(htmlspecialchars(print_r($1, true), ENT_QUOTES, "UTF-8")); ?>',
+
+        // CSS seguro (apenas arquivos locais permitidos)
+        '<?php if (preg_match("/^[a-zA-Z0-9_\-\/\.]+\\.css$/", "$1")): ?><link rel="stylesheet" href="<?php echo htmlspecialchars("$1", ENT_QUOTES, "UTF-8"); ?>"><?php endif; ?>',
+
+        // JS seguro (apenas arquivos locais permitidos)
+        '<?php if (preg_match("/^[a-zA-Z0-9_\-\/\.]+\\.js$/", "$1")): ?><script src="<?php echo htmlspecialchars("$1", ENT_QUOTES, "UTF-8"); ?>"></script><?php endif; ?>',
+
         '<input type="hidden" name="_csrf_token" id="_csrf_token" value="<?php echo htmlspecialchars($this->vars[\'csrf_token\'], ENT_QUOTES, \'UTF-8\'); ?>">',
+
         '<?php echo date("Y"); ?>',
-        '<?php for($i=$1; $i<=$2; $3++): ?>',
-        '<?php endfor; ?>',
-        '<?php $1 ?>'
+
+        // For seguro - apenas números inteiros
+        '<?php if (is_numeric($1) && is_numeric($2) && is_numeric($3)): for($i=$1; $i<=$2; $i+=$3): ?>',
+        '<?php endfor; endif; ?>',
     ];
 
     public static function getPatterns(): array
--- tests/Core/ViewsTest.php
+++ tests/Core/ViewsTest.php
@@ -12,7 +12,7 @@ class ViewsTest extends TestCase
     public function testRenderReturnsString()
     {
         ob_start();
-    new \Core\View\View('template', ['nome' => 'Lucas']);
+    new \Core\View\View('template', ['nome' => 'Lucas'],['/assets/css/estilo.css'], ['/assets/js/main.js']);
     $output = ob_get_clean();
 
     $this->assertStringContainsString('Olá Lucas', $output);
