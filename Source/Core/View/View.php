86ef1ac80f5360948ae12e61e3ece09f7d20f125
lucasmcar <lmc.lucas90@gmail.com>
2025-06-22 20:51:14 -0300

    Fixed bug render type module js

--- Source/Core/View/View.php
+++ Source/Core/View/View.php
@@ -9,9 +9,9 @@ use Core\View\Registers\Register;
 
 use function Core\view_base_path;
 
-class View 
+class View
 {
-    
+
     private $vars = [];
     private $layout;
     private $styles = [];
@@ -21,9 +21,8 @@ class View
     {
         $this->setLayout($layout);
         $this->vars['csrf_token'] = Csrf::generateToken();
-        foreach($vars as $name => $value){
-           $this->assignArray($name, $value);
-            
+        foreach ($vars as $name => $value) {
+            $this->assignArray($name, $value);
         }
 
         // Adiciona estilos e scripts específicos da página
@@ -43,7 +42,7 @@ class View
         return $this->layout;
     }
 
-    
+
     private function assignArray($name, $value)
     {
         $this->vars[$name] = $value;
@@ -52,17 +51,17 @@ class View
     public function render($template)
     {
         //$templatePath = dirname(__DIR__, 3) .'/resources/views/' . $template . '.tpl';
-        if(defined('PROJECT_VIEW_PATH')){
-            $templatePath = realpath(view_base_path().'/' . $template . '.tpl');
+        if (defined('PROJECT_VIEW_PATH')) {
+            $templatePath = realpath(view_base_path() . '/' . $template . '.tpl');
         } else {
-            $templatePath = realpath(view_base_path().'/resources/views/' . $template . '.tpl');
+            $templatePath = realpath(view_base_path() . '/resources/views/' . $template . '.tpl');
         }
-        
+
         if (!file_exists($templatePath)) {
             throw new \Exception("Template $templatePath not found!");
         }
         // Carregar arquivos de estilos e scripts específicos da página
-        
+
 
         extract($this->vars);
 
@@ -73,28 +72,27 @@ class View
 
         if ($this->layout) {
             //$layoutPath = dirname(__DIR__, 3).'/resources/views/layouts/' . $this->layout . '.tpl';
-            if(defined('PROJECT_VIEW_PATH')){
-                 $layoutPath = realpath(view_base_path()."/layouts/" . $this->layout . '.tpl');
+            if (defined('PROJECT_VIEW_PATH')) {
+                $layoutPath = realpath(view_base_path() . "/layouts/" . $this->layout . '.tpl');
             } else {
-                 $layoutPath = realpath(view_base_path().'/resources/views/layouts/' . $this->layout . '.tpl');
+                $layoutPath = realpath(view_base_path() . '/resources/views/layouts/' . $this->layout . '.tpl');
             }
-           
-    
+
+
             if (!file_exists($layoutPath)) {
                 throw new \Exception("Layout $layoutPath not found!");
             }
 
             $styles = $this->generateStyles();
             $scripts = $this->generateScripts();
-    
+
             // Include the content in the layout
-             // Replace the {{ $content }} placeholder with the actual content
+            // Replace the {{ $content }} placeholder with the actual content
             $layoutContent = file_get_contents($layoutPath);
             $layoutContent = str_replace('{{! $styles }}', $styles, $layoutContent);
             $layoutContent = str_replace('{{! $scripts }}', $scripts, $layoutContent);
             $layoutContent = str_replace('{{ $content }}', $content, $layoutContent);
             $content = $layoutContent;
-        
         }
         $parsedContent = $this->parse($content);
         // Render the parsed content within a safe context
@@ -104,16 +102,16 @@ class View
     private function parse($content)
     {
         $patterns = Register::getPatterns();
-        
+
         $replacements = Register::getReplacements();
-        
+
         return preg_replace($patterns, $replacements, $content);
     }
 
     private function renderContent($content, $vars)
     {
 
-        $vars['styles'] = $this->generateStyles();  
+        $vars['styles'] = $this->generateStyles();
         $vars['scripts'] = $this->generateScripts();
 
         extract($vars);
@@ -136,11 +134,28 @@ class View
         return $output;
     }
 
-    private function generateScripts()
+    /*private function generateScripts()
     {
         $output = "";
         foreach ($this->scripts as $script) {
             $output .= "<script src='" . \base_url($script) . "'></script>\n";
+        }
+        return $output;
+    }*/
+
+    private function generateScripts()
+    {
+        $output = "";
+        foreach ($this->scripts as $script) {
+            if (is_array($script)) {
+                $src = \base_url($script['src']);
+                $type = isset($script['type']) ? $script['type'] : 'text/javascript';
+            } else {
+                $src = \base_url($script);
+                $type = 'text/javascript';
+            }
+
+            $output .= "<script type='$type' src='$src'></script>\n";
         }
         return $output;
     }
